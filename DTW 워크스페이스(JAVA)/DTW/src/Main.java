6a2d45dd5351575559f7edce4dbda150c0b105f9
hanjun148 <hanjun148@naver.com>
2017-05-22 21:42:41 +0900

    주석 예쁘게 달기
    오류 검사 해야 하고 코드 잘 짜진건지 점검 필요@!

--- DTW 워크스페이스(JAVA)/DTW/src/Main.java
+++ DTW 워크스페이스(JAVA)/DTW/src/Main.java
@@ -10,75 +10,85 @@ public class Main {
 		float num;
 
 		// There were several ways of File handling ... so I did two kinds of
-		// them. (그냥 두개의 방법으로 파일입출력 한거)
-		// arrayList에 저장
+		// them. (그냥 두개의 방법으로 파일입출력 한거 for study...?) 
+		
+		// 1_save Server's Data to an 'arrayList'.
 		ArrayList<Character> arrayList = new ArrayList<Character>();
 		String filename = "저장데이터.txt";
 		FileInputStream fis = new FileInputStream(filename);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(fis));
 		while ((num = reader.read()) != -1) {
+			//read only 1 and 0 (not '\n'...so on)
 			if (num == '1' || num == '0')
 				arrayList.add((char) num);
 		}
 		reader.close();
 		fis.close();
-
-		// arrayList2에 저장
+		
+		
+		// 2_save Client's Data to an 'arrayList2'.
 		ArrayList<Character> arrayList2 = new ArrayList<>();
 		FileReader reader1 = new FileReader("체크.txt");
 		while ((num = reader1.read()) != -1) {
+			//read only 1 and 0 (not '\n'...so on)
 			if (num == '1' || num == '0')
 				arrayList2.add((char) num);
 		}
 		reader1.close();
-
+		
+		
 		// set arrayList to n1 & arrayList2 to n2
+		// I'm going to divide server's data(n1) to several segmentedN1 and compare with client's data(n2)! 
 		float n1[] = new float[arrayList.size()];
 		float n2[] = new float[arrayList2.size()];
 		float segmentedN1[] = new float[((int) (n2.length))];
-		int segSize = n1.length / ((int) (n2.length));
+		int segSize = n1.length / n2.length;
+		//change char'010111011' to float'010111011'...
 		for (int i = 0; i < arrayList2.size(); i++) {
 			n2[i] = arrayList2.get(i) - '0';
 		}
-		System.out.println("n2 길이 : " + n2.length + ", n1 : " + n1.length);
+		System.out.println("n1의 길이 : " + n1.length+", n2의 길이 : " + n2.length );
 
+		
 		// compare each segmentedN1 with n1
-		ArrayList<Integer> minArr = new ArrayList<>();
 		int i = 0;
 		double min = 100;
 		int minSeg = 0;
 		int tmpSegSize = segSize;
+		ArrayList<Integer> minSegArr = new ArrayList<>();
 		while (tmpSegSize > 0) {
-			for (int j = 0; j < ((int) (n2.length)); j++) {
+			for (int j = 0; j < ((int) (n2.length)); j++,i++) {
 				segmentedN1[j] = arrayList.get(i) - '0';
-				i++;
 			}
 			DTW2 dtw = new DTW2(segmentedN1, n2);
-			//finding 3 minSeg which makes minimum min value 
+			//finding any minSeg which makes minimum min value
+			//I wanted to find 3 minSegs but I cannot focus on this right now!!!!
 			if (min > dtw.getDistance()) {
 				min = dtw.getDistance();
 				minSeg = tmpSegSize;
-				minArr.add(0, minSeg);
+				minSegArr.add(0, minSeg);
 			}
 			tmpSegSize--;
 		}
 
-		// go to minimum value stage!
+		
+		// go to one of minimum value stage!
 		int tmpi;
-		for (int l = 0; l < minArr.size(); l++) {
+		for (int l = 0; l < minSegArr.size(); l++) {
 			int k = 0;
-			i = (segSize - minArr.get(l) - 1) * n2.length + n2.length / 3;
-			if (minArr.get(l) == segSize) { // 배열의 첫부분일 때 (minArr의 값은 왼쪽이 가장
-				// 큰 segSize의 크기를 가짐)
-				k = 2; // Do this 'for' 3 time!
-				i = 0;
-			} else if (minArr.get(l) == 1) {
-				k = 2;
+			i = (segSize - minSegArr.get(l) - 1) * n2.length + n2.length / 3;	//3등분해서 지나감
+			//Because the 'minSegArr == 1' means the last segment of the n1. 
+			if (minSegArr.get(l) == 1) {
+				k = 2; // Do this 'for'loop just 3 time! 
 			}
-			System.out.println(segSize + "," + minArr.get(l) + ", " + i);
+			else if (minSegArr.get(l) == segSize) { //if the similarity part is the first segment of the n1.
+				k = 2;
+				i = 0;
+			} 
+			//to see where 	is similarity part~
+			System.out.println("segSize : "+segSize + ", minSegArr : " + minSegArr.get(l) + ", i : " + i);
 			for (; k < 5; k++) {
 				tmpi = i;
-
 				for (int j = 0; j < ((int) (n2.length)); j++, tmpi++) {
 					segmentedN1[j] = arrayList.get(tmpi) - '0';
 				}
