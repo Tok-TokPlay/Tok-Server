20638a2ba8da52541dc52818875ca8dc20fcc2db
Unknown <youngwoon96@naver.com>
2017-05-24 01:33:34 +0900

    File automatically create

--- DTW 워크스페이스(JAVA)/DTW/src/FileClass.java
+++ DTW 워크스페이스(JAVA)/DTW/src/FileClass.java
@@ -1,15 +1,20 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.*;
+import java.util.Random;
 
 public class FileClass {
 
 	private String filename;
 	void randomMapping(int numOfFile) throws IOException{
 		int arr[] = new int[1000];
+		Random rand = new Random();
+		
 		for(int i=0;i<numOfFile; i++){
 			filename = i+".txt";
 			File file = new File(filename);
@@ -18,10 +23,10 @@ public class FileClass {
 			FileWriter fw = new FileWriter(file);
 			for(int j=0;j<200;j++){
 				for(int k=0;k<1000;k++){
-					ran
+					fw.write((char)rand.nextInt(2)+'0');
 				}
 			}
-			
+			fw.close();
 		}
 	}
 	void fileOpen(int numOfFile) throws FileNotFoundException{
