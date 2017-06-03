dafcc76bac1d5101a11653cd1e6cba8ecbd84cc1
최현준 <chj878194@naver.com>
2017-06-04 02:28:54 +0900

    Tok Server Skeleton made.
    Tok Server Skeleton made.

--- Server code/TCPServer/src/TokServer.java
+++ Server code/TCPServer/src/TokServer.java
@@ -7,5 +7,28 @@
  * 3. Communication with Android Client. Listen User input and Talk What is this music.
  * */
 public class TokServer {
+	private static TCPConfig serverConfig;
+	private static MusicDataBase musicDB;
+
+	public static int main(String[] arg) {
+		if(arg.length == 3) {
+			serverConfig = new TCPConfig(arg[0], Integer.parseInt(arg[1]), arg[2]);
+		}
+		else {
+			System.out.println("Please input in order with IP, Port, DB_Directory please...");
+			return 0;
+		}
+		
+		return 0;
+	}
 	
+	private void listenClient(){
+		// Listen and find music at DB.
+		Thread desktopServerThread = new Thread(new TCPServer(serverConfig, musicDB));
+		desktopServerThread.start();
+	}
+	
+	private void addMusic(String musicName, String musicSinger, String fileName)	{
+		musicDB.addMusic(musicName, musicSinger, fileName);
+	}
 }
