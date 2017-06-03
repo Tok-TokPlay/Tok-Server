f4b794b43f379232e1e20429ccb67d70982ae2e1
최현준 <chj878194@naver.com>
2017-06-03 23:06:32 +0900

    Data class modified.
    Data class modified.

--- Server code/TCPServer/src/Data.java
+++ Server code/TCPServer/src/Data.java
@@ -5,12 +5,23 @@ public class Data implements Serializable {
 	private static final long serialVersionUID = 878194L;
 	//serialVersionUID is for check serialize ID.
 	//This may help flexible checking to Old and new version.
-	private ArrayList<Integer> userBeat = new ArrayList<>();
+	private ArrayList<Integer> list = new ArrayList<>();
 	
 	public ArrayList<Integer> getList(){
-        return userBeat;
+        return list;
     }
+	
     public void setList(ArrayList<Integer> list){
-        this.userBeat=list;
+        this.list=list;
+    }
+    
+    @Override
+    public String toString()	{
+		//Change Data list into String.
+    	String returnValue = "";
+    	for(int i = 0; i < this.getList().size(); i++){
+    		returnValue = returnValue.concat(getList().get(i).toString());
+    	}
+    	return returnValue;
     }
 }
