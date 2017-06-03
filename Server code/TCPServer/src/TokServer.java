62bcedfdb9588fec50ec7074a80c4dc6eb823974
최현준 <chj878194@naver.com>
2017-06-04 00:24:13 +0900

    Real Tok-Server Class define.
    Real Tok-Server Class define.
    - Server need to be act 3 action for server administrator.

---
+++ Server code/TCPServer/src/TokServer.java
@@ -0,0 +1,11 @@
+/**
+ * @author Hyun Joon Choi
+ * This code act 3 function.
+ * 1. Make music DB with given python code or bash / batch shell.
+ * - Insert to Database.
+ * 2. Check and Search music with user input.
+ * 3. Communication with Android Client. Listen User input and Talk What is this music.
+ * */
+public class TokServer {
+	
+}
